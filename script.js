d25fd64162150e5139cd271e83a8e968e6992aa8
Kingsosu <kingsosuolale@gmail.com>
2023-11-04 11:40:52 +0100

    work on navigation bar links event listener

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ const menuToggle = document.getElementById('menu-toggle');
 const navBarLink = document.getElementById('nav-bar-link');
 
 menuToggle.addEventListener('click', () => {
-    navBarLink.classList.toggle('active')
+    navBarLink.classList.toggle("active")
 });
+
+
+const navLink = document.querySelectorAll('.nav-bar-link a');
+
+navLink.forEach(link => {
+    link.addEventListener('click', () => {
+        navBarLink.classList.remove("active")
+    });
+});
+
